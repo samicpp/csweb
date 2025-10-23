6f83f795dd4c681961ce8aba50762748487469d8
samic++ <samichentouf+git2@gmail.com>
2025-10-23 15:53:07 +0200

    setup prokect

---
+++ web/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Samicpp.Web;
+// See https://aka.ms/new-console-template for more information
+using Samicpp.Http;
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
