0490657b370f078a04c1bcb0eca919f5bbe1dd8a
samic++ <samichentouf+git2@gmail.com>
2025-11-05 14:56:27 +0100

    change install script & rename systemd service file

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo mkdir -p /opt/csweb
 sudo chown -R $USER:$USER /opt/csweb
-sudo ln -s `pwd`/csweb.service /etc/systemd/system
 cd /opt/csweb
 git clone https://github.com/samicpp/csweb --recurse-submodules .
+sudo ln -s `pwd`/csweb.service /etc/systemd/system
